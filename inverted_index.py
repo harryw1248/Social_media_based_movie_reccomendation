6bde80422818e99d0d370b790c55bbe65ea6f2e0
vahluw <30989722+vahluw@users.noreply.github.com>
2019-04-06 18:26:02 -0400

    added synopsis and tags to document for each movie

--- inverted_index.py
+++ inverted_index.py
@@ -5,23 +5,14 @@ import pickle
 import string
 import time
 import nltk
-import getpass
-import calendar
 import os
-import platform
 import sys
-import urllib.request
-
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 # Class that holds a posting list, length of posting list, and max term frequency for any term in the inverted index
-class PostingList():
+class PostingList:
 
     def __init__(self):
         self.posting_list = list()
@@ -30,11 +21,12 @@ class PostingList():
 
 
 # For each element in a posting list, the document ID and the corresponding term frequency are stored
-class PostingData():
+class PostingData:
 
     def __init__(self, movieID_in, tf_in):
         self.movieID = movieID_in
         self.tf = tf_in
+
 
 # When the dictionary of document weights is created, each document maps to a vector of weights and its document length
 class SimilarityData:
@@ -46,6 +38,7 @@ class SimilarityData:
 # Global variable that I use to store document term weightings so I could obey function signatures
 # Holds mapping of movieID to weights vector as well as length of document
 doc_term_weightings = dict()
+
 
 # Computes the document vector weights using weighting scheme #1: TF-IDF
 def computeDocWeightsTFIDF(inverted_index, num_files):
@@ -69,26 +62,6 @@ def computeDocWeightsTFIDF(inverted_index, num_files):
         index += 1  # Increment index to move to next term
 
     for movieID in range(1, num_files + 1):   # Finish calculating doc lengths
-        doc_term_weightings[movieID].doc_length = math.sqrt(doc_term_weightings[movieID].doc_length)
-
-# Computes the document vector weights using weighting scheme #2: Best-Weighting Probabilistic Weight
-def computeDocWeightsBWPW(inverted_index, num_files):
-    # Intializes global variable holding document term weightings to SimilarityData() objects
-    for movieID in range(1, num_files + 1):
-        doc_term_weightings[movieID] = SimilarityData(len(inverted_index))
-
-    index = 0
-    for term in inverted_index:  # Iterates through each term in the inverted index
-        max_tf = inverted_index[term].max_tf + 0.0  # Finds the maximum term frequency for each term
-        for posting_data in inverted_index[term].posting_list:  # Iterates through each document in each posting list
-            tf = posting_data.tf + 0.0
-            movieID = posting_data.movieID
-            weight = 0.5 + 0.5 * (tf/max_tf)    # Weight is calculated as (0.5 + 0.5 * (tf/max_tf))
-            doc_term_weightings[movieID].weights[index] = weight
-            doc_term_weightings[movieID].doc_length += weight * weight    # Running total of document length updated
-        index += 1  # Increment index to move to next term
-
-    for movieID in range(1, num_files + 1):   # Finally, each document length is finally calculated by taking square root
         doc_term_weightings[movieID].doc_length = math.sqrt(doc_term_weightings[movieID].doc_length)
 
 
@@ -146,22 +119,6 @@ def calculateQueryDataTFIDF(query_weights, query_appearances, query_length, inve
     return query_length
 
 
-def calculateQueryDataBWPW( query_weights, query_appearances, query_length, inverted_index):
-    num_files = len(doc_term_weightings) + 0.0
-
-    # Iterate through each term in the query vector and assign nonzero weight if the term appears in inverted index
-    # weight = log ((N - n)/n), where N = number of documents and n = document frequency
-    for query_term in query_appearances:
-        if query_term in inverted_index:
-            index_of_word = inverted_index.keys().index(query_term)     # Since ordered dict, calculate index of term
-            num_postings = inverted_index[query_term].length + 0.0      # Document frequency
-            query_weights[index_of_word] = math.log10((num_files - num_postings)/num_postings)
-            query_length += query_weights[index_of_word] * query_weights[index_of_word]
-
-    query_length = math.sqrt(query_length)                              # Calculate final query length
-    return query_length
-
-
 # Returns ordered ranking of retrieved documents
 def calculateDocumentSimilarity(query_appearances, inverted_index, query_weights, query_length):
     docs_with_at_least_one_matching_query_term = set()
@@ -181,18 +138,8 @@ def calculateDocumentSimilarity(query_appearances, inverted_index, query_weights
     return docs_with_scores
 
 
-def indexDocument(document, doc_weighting_scheme, inverted_index, movieID, use_kaggle = False):
+def indexDocument(document, inverted_index, movieID):
     tokens = nltk.word_tokenize(document)
-
-    if use_kaggle:
-        pickle_in = open("kaggle.pickle", "rb")
-        kaggle_dict = pickle.load(pickle_in)
-
-        for (movieID, title) in index_to_movies:
-            if title in kaggle_dict.keys():
-                for keyword in kaggle_dict[title]:
-                    tokens.append(keyword)
-
     tokens = [x for x in tokens if x not in string.punctuation]
     tokens = removeStopWords(tokens)  # Remove the stopwords
     tokens = stemWords(tokens)      # PorterStemmer
@@ -203,7 +150,7 @@ def indexDocument(document, doc_weighting_scheme, inverted_index, movieID, use_k
     createInvertedIndex(inverted_index, movieID, tokens)   # Create the inverted index
 
 
-def retrieveDocuments(query, inverted_index, doc_weighting_scheme, query_weighting_scheme):
+def retrieveDocuments(query, inverted_index):
     tokens = nltk.word_tokenize(query)
     tokens = [x for x in tokens if x not in string.punctuation]
     query_tokens = removeStopWords(tokens)  # Remove the stopwords
@@ -222,117 +169,65 @@ def retrieveDocuments(query, inverted_index, doc_weighting_scheme, query_weighti
     query_length = 0.0
 
     # Use query weighting scheme to appropriately calculate query term weights
-    if query_weighting_scheme == "tfidf":
-        query_length = calculateQueryDataTFIDF(query_weights, query_appearances, query_length, inverted_index)
-    elif query_weighting_scheme == "bwpw":
-        query_length = calculateQueryDataBWPW(query_weights, query_appearances, query_length, inverted_index)
-
-    #pickle_out = open("query_vector.pickle", "wb")
-    #pickle_out.write(query_weights)
-    #pickle_out.close()
+    query_length = calculateQueryDataTFIDF(query_weights, query_appearances, query_length, inverted_index)
 
     # After calculating query weights and length, returns ranked list of documents by calculating similarity
     return calculateDocumentSimilarity(query_appearances, inverted_index, query_weights, query_length)
 
 
-def get_metadata(synopsis_image_info, index_to_movies):
-    """ Logging into our own profile """
+def correct_title_exceptions(title):
+    title = title.replace(" ", "_")
 
-    # try:
-    global driver
+    if ":" in title:
+        title = title.replace(":", "")
 
-    options = Options()
+    if "'" in title:
+        title = title.replace("'", "")
 
-    chromedriver = "/Users/Vinchenzo4335/PycharmProjects/EECS486/Final_Project/chromedriver"
-    os.environ["webdriver.chrome.driver"] = chromedriver
-    # chrome_options.add_argument("--headless")
-    options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    if "-" in title:
+        title = title.replace("-", "")
 
-    #  Code to disable notifications pop up of Chrome Browser
-    options.add_argument("--disable-notifications")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--mute-audio")
-    # options.add_argument("headless")
+    if "&" in title:
+        title = title.replace("&", "and")
 
-    driver = webdriver.Chrome(executable_path=chromedriver, options=options)
-    num_found = 0
-    movie_num = 1
+    if "__" in title:
+        title = title.replace("__", "_")
 
-    for movieID in index_to_movies:
-        movie_title = index_to_movies[movieID]
+    if title == "frozen_(disney)":
+        title = "frozen_2013"
 
-        if ", The" in movie_title:
-            index = movie_title.find(", The")
-            movie_title = "The " + movie_title[0: index]
+    elif title == "the_avengers_(2012)":
+        title = "marvels_the_avengers"
 
-        if ", a" in movie_title:
-            index = movie_title.find(", a")
-            movie_title = "A " + movie_title[0: index]
+    elif title == "star_wars_the_phantom_menace":
+        title = "star_wars_episode_i_the_phantom_menace"
 
-        movie_title = movie_title.lower()
-        movie_title = movie_title.replace(" ", "_")
+    elif title == "star_wars_attack_of_the_clones":
+        title = "star_wars_episode_ii_attack_of_the_clones"
 
-        if ":" in movie_title:
-            movie_title = movie_title.replace(":", "")
+    elif title == "star_wars_revenge_of_the_sith":
+        title = "star_wars_episode_iii_revenge_of_the_sith"
 
-        if "'" in movie_title:
-            movie_title = movie_title.replace("'", "")
+    elif title == "star_wars_a_new_hope":
+        title = "star_wars"
 
-        if "-" in movie_title:
-            movie_title = movie_title.replace("-", "")
+    elif title == "star_wars_the_empire_strikes_back":
+        title = "empire_strikes_back"
 
-        if "&" in movie_title:
-            movie_title = movie_title.replace("&", "and")
+    elif title == "star_wars_return_of_the_jedi":
+        title = "star_wars_episode_vi_return_of_the_jedi"
 
-        if "__" in movie_title:
-            movie_title = movie_title.replace("__", "_")
+    elif title == "star_wars_the_force_awakens":
+        title = "star_wars_episode_vii_the_force_awakens"
 
-        try:
-            driver.get("https://www.rottentomatoes.com/m/" + movie_title)
-            synopsis = driver.find_element_by_id('movieSynopsis').text
-            url = driver.find_element_by_class_name('posterImage').get_attribute('src')
+    elif title == "semipro":
+        title = "semi_pro"
 
-            if url is None:
-                url = "No image available."
+    return title
 
-            synopsis_image_info[movieID] = (synopsis, url)
-            num_found += 1
-        except:
-            if movie_title[0:4] == "the_":
-                try:
-                    movie_title = movie_title[4: len(movie_title)]
-                    driver.get("https://www.rottentomatoes.com/m/" + movie_title)
-                    synopsis = driver.find_element_by_id('movieSynopsis').text
-                    url = driver.find_element_by_class_name('posterImage').get_attribute('src')
 
-                    if url is None:
-                        url = "Image not found."
-
-                    synopsis_image_info[movieID] = (synopsis, url)
-                    num_found += 1
-                except:
-                    synopsis_image_info[movieID] = ("No synopsis found.", "No image available.")
-                    print(str(movie_num) + " " + movie_title + " NOT FOUND")
-            else:
-                synopsis_image_info[movieID] = ("No synopsis found.", "No image available.")
-                print(str(movie_num) + " " + movie_title + " NOT FOUND")
-
-        movie_num += 1
-
-    print(num_found)
-
-    pickle_out1 = open("synopsis_image_info.pickle", "wb")
-    pickle.dump(synopsis_image_info, pickle_out1)
-    pickle_out1.close()
-
-    pickle_out2 = open("index_to_movies.pickle", "wb")
-    pickle.dump(index_to_movies, pickle_out2)
-    pickle_out2.close()
-
-def create_data(doc_weighting_scheme, inverted_index, num_files, synopsis_image_info, index_to_movies, use_kaggle=False):
+def create_data(inverted_index, num_files, synopsis_image_info, index_to_movies):
     current_index = 0
-
-    #get_metadata(synopsis_image_info, index_to_movies)
     global driver
 
     options = Options()
@@ -372,9 +267,8 @@ def create_data(doc_weighting_scheme, inverted_index, num_files, synopsis_image_
             movie_title = "The " + movie_title[0: index]
 
         if ", A" in movie_title:
-            index = movie_title.find(", a")
+            index = movie_title.find(", A")
             movie_title = "A " + movie_title[0: index]
-
 
         print(movie_title + ", Index: " + str(current_index))
 
@@ -387,50 +281,7 @@ def create_data(doc_weighting_scheme, inverted_index, num_files, synopsis_image_
             for tag in movie_attributes[movie_title]:
                 script = script + tag
 
-        movie_title = movie_title.replace(" ", "_")
-
-        if ":" in movie_title:
-            movie_title = movie_title.replace(":", "")
-
-        if "'" in movie_title:
-            movie_title = movie_title.replace("'", "")
-
-        if "-" in movie_title:
-            movie_title = movie_title.replace("-", "")
-
-        if "&" in movie_title:
-            movie_title = movie_title.replace("&", "and")
-
-        if "__" in movie_title:
-            movie_title = movie_title.replace("__", "_")
-
-        if movie_title == "frozen_(disney)":
-            movie_title = "frozen_2013"
-
-        elif movie_title == "the_avengers_(2012)":
-            movie_title = "marvels_the_avengers"
-
-        elif movie_title == "star_wars_the_phantom_menace":
-            movie_title = "star_wars_episode_i_the_phantom_menace"
-
-        elif movie_title == "star_wars_attack_of_the_clones":
-            movie_title  = "star_wars_episode_ii_attack_of_the_clones"
-
-        elif movie_title == "star_wars_revenge_of_the_sith":
-            movie_title  = "star_wars_episode_iii_revenge_of_the_sith"
-
-        elif movie_title == "star_wars_a_new_hope":
-            movie_title  = "star_wars"
-
-        elif movie_title == "star_wars_the_empire_strikes_back":
-            movie_title  = "empire_strikes_back"
-
-        elif movie_title == "star_wars_return_of_the_jedi":
-            movie_title  = "star_wars_episode_vi_return_of_the_jedi"
-
-        elif movie_title == "star_wars_the_force_awakens":
-            movie_title  = "star_wars_episode_vii_the_force_awakens"
-
+        movie_title = correct_title_exceptions(movie_title)
 
         try:
             driver.get("https://www.rottentomatoes.com/m/" + movie_title)
@@ -467,16 +318,13 @@ def create_data(doc_weighting_scheme, inverted_index, num_files, synopsis_image_
 
         movie_num += 1
 
-        indexDocument(script, doc_weighting_scheme, inverted_index, movieID, use_kaggle)  # Update the inverted index
+        indexDocument(script, inverted_index, movieID)  # Update the inverted index
         file.close()
         num_files += 1
         current_index = current_index + 1
 
     # Once inverted index is complete, compute document weights using the appropriate weighting scheme
-    if doc_weighting_scheme == "tfidf":
-        computeDocWeightsTFIDF(inverted_index, num_files)
-    elif doc_weighting_scheme == "bwpw":
-        computeDocWeightsBWPW(inverted_index, num_files)
+    computeDocWeightsTFIDF(inverted_index, num_files)
 
     pickle_out1 = open("inverted_index.pickle", "wb")
     pickle.dump(inverted_index, pickle_out1)
@@ -489,24 +337,17 @@ def create_data(doc_weighting_scheme, inverted_index, num_files, synopsis_image_
     pickle_out3.close()
 
 
-
-
 if __name__ == '__main__':
 
-    queries = "Posts.txt"
-    create_index = True
-    read_in_synopsis_info = True
-    use_kaggle = False
+    queries, create_index, doc_folder = sys.argv[1], sys.argv[2], sys.argv[3]
 
     index_to_movies = dict()
     synopsis_image_info = dict()
-    doc_weighting_scheme = "tfidf"
     inverted_index = collections.OrderedDict()  # Inverted index is ordered dictionary to allow for consistent indexing
-    doc_folder = "Testing/"
     num_files = 0
 
-    if create_index:
-        create_data(doc_weighting_scheme, inverted_index, num_files, synopsis_image_info, index_to_movies, use_kaggle)
+    if create_index == "create_index":
+        create_data(inverted_index, num_files, synopsis_image_info, index_to_movies)
 
     else:
         pickle_in = open("inverted_index.pickle", "rb")
@@ -516,33 +357,21 @@ if __name__ == '__main__':
         pickle_in = open("index_to_movies.pickle", "rb")
         index_to_movies = pickle.load(pickle_in)
 
-    '''
-    if read_in_synopsis_info:
-        get_metadata(synopsis_image_info, index_to_movies)
-
-    else:
-        pickle_in = open("synopsis_image_info.pickle", "rb")
-        synopsis_image_info = pickle.load(pickle_in)
-
-    '''
-
     t0 = time.time()
-    query_weighting_scheme = "tfidf"
+    print("Searching for your recommended movies...\n")
 
     query_doc = open(os.getcwd() + "/" + queries, 'r')  # Open the file
     out_file = open(os.getcwd() + "/" + "recommendations.txt", 'w')
     line = query_doc.read()
-    query_num = 1
 
-    docs_with_scores = retrieveDocuments(line, inverted_index, doc_weighting_scheme, query_weighting_scheme)
+    docs_with_scores = retrieveDocuments(line, inverted_index)
     ordered_list = sorted(docs_with_scores.items(), key=lambda x: x[1])  # Order the list
 
-
-    rank = 1
     print("\nTotal time to make recommendation:" + str(time.time() - t0) + " seconds")    # Print computation time
     print("Your Top 10 Movie Recommendations:\n")
     out_file.write("Your Top 10 Movie Recommendations:\n")
 
+    rank = 1
     for (movieID, score) in reversed(ordered_list):  # Print each ranking member to the output file
         movie_title = index_to_movies[movieID]
 
