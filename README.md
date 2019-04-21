0397ae36edf6d2067b8bc762647eb322fd92654f
Anthony <aliangdw@umich.edu>
2019-04-21 16:41:08 -0400

    added readme

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
 # EECS_486_final_project
 
-Movie recommendation
+# A Social Media Based Recommendation System
+
+## Dependencies
+
+Run this command to install the dependencies needed for this project
+`pip3 install -r requirements.txt`
+
+## How to run:
+
+(Steps 1 and 2 are necessary to run scraper)
+1. Install `chromedriver` from https://sites.google.com/a/chromium.org/chromedriver/
+2. Move chromedriver to root directory of project
+3. Load the webapp: `python3 app.py`. This will load the flask app on localhost. Navigate
+to the path specified in the terminal.
+4. You have reached the home page. Type in username and password to your Facebook.
+5. Fill in relative or absolute path to Facebook and Twitter (optional) profile. `e.g. /shaeqahmed`.
+6. Hit the Generate Recommendation button. You should see a Chrome window pop up and automate the Facebook scraping.
+7. Generating recommendations usually takes ~45seconds - a minute. It should redirect to another route once the recommendations are generated.
+8. Follow the instructions on the side panel of the page to submit feedback and update your recommendations.
+
+
+![alt text](gui.png "Logo Title Text 1")
+
+
+## Repo Tree
+```
+├── PorterStemmer.py
+├── README.md
+├── app.py
+├── chromedriver
+├── collaborative_filtering.py
+├── doc_term_weightings.pickle
+├── eval_metrics.csv
+├── evaluation_graphs.py
+├── index_to_movies.pickle
+├── inverted_index.pickle
+├── inverted_index.py
+├── kendall_tau_data.pickle
+├── mean_average_precision_data.pickle
+├── movie_attributes.pickle
+├── mrr_data.pickle
+├── past_feedback.pickle
+├── preprocess.py
+├── query_appearances.pickle
+├── query_weights.pickle
+├── r_precision_data.pickle
+├── recs.pickle
+├── relevanceFeedback.py
+├── scraper.py
+├── static
+│   ├── animate.min.css
+│   ├── formstyle.css
+│   ├── home_style.css
+│   ├── images
+│   │   └── avengers.jpg
+│   └── rec_style.css
+├── synopsis_image_info.pickle
+├── templates
+│   ├── homepage.html
+│   └── recommendations.html
+└── utils
+    ├── __init__.py
+    ├── auth.py
+    ├── story.py
+```
--- scraper.py
+++ scraper.py
@@ -13,6 +13,13 @@ service_id = 'eecs486project'
 session = HTMLSession()
 driver = None
 
+'''
+    Gets all the tweet content of a given user profile
+    Args:
+        profile (str):
+    Return:
+        tweets (list):  a list of all the tweets
+'''
 def get_tweets(user):
     url = f'https://twitter.com/i/profiles/show/{user}/timeline/tweets'
     r = session.get(url)
@@ -35,10 +42,15 @@ def get_tweets(user):
     tweets = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", tweets)
     return tweets
 
+'''
+    Helper function to scroll through user profile in Facebook
+'''
 def scroll():
     current_scrolls = 0
     prev_height, current_height = -1, 0
     fails = 0
+
+    # Scroll until we reach the bottom of the page
     while True:
         if prev_height == current_height:
           fails += 1
@@ -58,6 +70,15 @@ def scroll():
         current_scrolls += 1
     return
 
+'''
+    Main scrape function for Facebook.
+    Writes scraped posts into a text file
+    Args:
+        fb_profile (str):
+        twittere_profile (str):
+    Returns:
+        None
+'''
 def scrape(fb_profile, twitter_profile):
     url = "https://www.facebook.com/" + fb_profile
     driver.get(url)
@@ -66,7 +87,9 @@ def scrape(fb_profile, twitter_profile):
     if not os.path.exists('data/'+fb_profile):
         os.mkdir(os.path.join('data', url.split('/')[-1]))
 
-    scroll()
+    scroll() # scroll to end of pagee
+
+    # Use selenium driver to find the elements in HTML DOM
     data = driver.find_elements_by_xpath('//div[@class="_5pcb _4b0l _2q8l"]')
     status = None
     with open("data/"+fb_profile+'/posts.txt', "w") as f:
@@ -89,6 +112,12 @@ def scrape(fb_profile, twitter_profile):
           f.write(" "+tweets)
     return
 
+'''
+    Login to Facebook in order to view other uses timeline
+    Uses keyring for password security
+    Args:
+        email (str): facebook  login email
+'''
 def login(email):
     global driver
     driver.get("https://www.facebook.com")
@@ -96,11 +125,19 @@ def login(email):
     driver.find_element_by_name('pass').send_keys(keyring.get_password(service_id, email))
     driver.find_element_by_id('loginbutton').click()
 
+'''
+    Main function for executing both scrapers. Open chromedriver
+    and execute scraper.
+    Args:
+        login_email (str):
+        fb_profile (str):
+        twitter_profile (str):
+        bin_path (str):
+'''
 def run_scraper(login_email, fb_profile, twitter_profile, bin_path):
     global driver
     options = Options()
     options.binary_location ="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
- # '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
 
     options.add_argument("--disable-notifications")
     options.add_argument("--disable-infobars")
